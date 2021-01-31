157638e77025b65201a0ca32f45def3bb43afc5d
unknown <singh.avvi98@gmail.com>
2021-02-01 00:32:36 +1100

    Server up and running with routers in controllers working

--- server.js
+++ server.js
@@ -18,10 +18,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-// ***!!!DEFINE ROUTE LOCATION BELOW!!!***
-// var routes = require("./controllers/burgerController.js");
+var routes = require("./controllers/routes");
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
