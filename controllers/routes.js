f250687b68b2f07bf052a06cb541d686b1429e20
unknown <singh.avvi98@gmail.com>
2021-02-01 11:24:39 +1100

    init rendering working

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Server working');
+    res.render("index")
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,26 +10,23 @@ const io = socketio(server);
 const PORT = process.env.PORT;
 const publicDirpath = path.join(__dirname, './public');
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(publicDirpath));
-
-// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 const routes = require("./controllers/routes");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+io.on('connection', () => {
+    console.log('New web-socket connection')
+})
+
 server.listen(PORT, () => {
-  // Log (server-side) when our server has started
   console.log(`Server now up and running on port ${PORT}`);
 });
